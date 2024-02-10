02ce16689f701cf0700e2ae304db1197c08d1eff
Oleg Sergeev <o.v.sergeyev@gmail.com>
2024-02-10 19:16:24 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script type="ts" setup>
+  console.log('Loaded')
+</script>
+
+<template>
+  <seciton>
+    <div>
+      <slot></slot>
+    </div>
+  </seciton>
+</template>
+
+<style scoped>
+
+</style>
