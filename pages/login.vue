a8e72c1def6450b6ffbdedfa0cb7f656fc14661d
Oleg Sergeev <o.v.sergeyev@gmail.com>
2024-02-11 01:28:21 +0300

    Главная страница и форма логина

---
+++ pages/login.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+  useHead({
+    title: "Login"
+  })
+
+  const emailRef = ref('');
+  const passwordRef = ref('');
+  const nameRef = ref('');
+
+  
+
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen w-full">
+    <div class="bg-sidebar w-1/4 p-5 rounded">
+      <h1 class="text-2xl font-bold text-center mb-5">Login</h1>
+
+      <form>
+        <UiInput
+          placeholder="Email"
+          type="email"
+          class="mb-3"
+          v-model="emailRef"
+        />
+        <UiInput
+          placeholder="Password"
+          type="password"
+          class="mb-3"
+          v-model="passwordRef"
+        />
+        <UiInput
+          placeholder="Name"
+          type="name"
+          class="mb-3"
+          v-model="nameRef"
+        />
+        <div class="flex items-center justify-center gap-5">
+          <UiButton type="button">Login</UiButton>
+          <UiButton type="button">Register</UiButton>
+        </div>
+      </form>
+
+    </div>
+  </div>
+</template>
